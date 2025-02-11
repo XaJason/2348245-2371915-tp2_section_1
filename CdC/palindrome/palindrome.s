2b572fa785a4e1a4c912d52d3015bfa9c08180d9
Gabriel Nanu <gabriel.nanu@polymtl.ca>
2025-02-11 16:52:36 -0500

    première version Gab

--- CdC/palindrome/palindrome.s
+++ CdC/palindrome/palindrome.s
@@ -1,3 +1,8 @@
+.data
+taille: .byte 0
+left: .byte 0
+right: .byte 0
+
 .text
 .globl isPalindromeASM
 isPalindromeASM:
@@ -6,7 +11,33 @@ movl %esp, %ebp
 pushl %ebx
 movl 8(%ebp), %esi #potential palindrome
 # DEBUT COMPLETION
+#compter nombre d'éléments
+#left = (%esi) départ
+#right -> left + 8* (taille-1) (on va dire) -> le but est d'accéder le dernier élément 
+#comparer les pointeurs 
+# décrémenter right et incrémenter left
+xorl %eax, %eax # il est a 0 
+cld
+compteur: 
+lodsb #déplace esi dans eax(al)
+cmp $0,%eax
+jz comparateur
+incb taille
+jmp compteur
 
+comparateur: 
+movb taille, %cl
+#edge case: taille nulle
+movb %cl, right
+decb right
+movb right, %al
+
+
+
+
+
+
+//jle
 
 # FIN COMPLETION
 # NE RIEN MODIFIER APRES CETTE LIGNE
